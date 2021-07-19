ae9ffbc488403fe35458e53fb96598f4b5bcd3d1
ArnavNikam <75774457+ArnavNikam@users.noreply.github.com>
2021-07-19 19:18:41 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -20,11 +20,23 @@ document.getElementById("Ques").innerHTML = Name1;
 document.getElementById("Ans").innerHTML = Name2;
 
 function send() {
-    word = document.getElementById("word").value;
+    document.getElementById("output").style.visibility; inerth;
+
+    word1 = document.getElementById("word1").value;
+
+    word2 = document.getElementById("word2").value;
+
+    Get_number_1 = localStorage.getItem("Inp1");
+
+    document.getElementById("number1").innerHTML = Get_number_1;
+
+    Get_number_2 = localStorage.getItem("Inp2");
+
+    document.getElementById("number2").innerHTML = Get_number_2;
 
     QuestionNumber = "<h4 id='Word'> Q."+ NewLL + "</h4>";
 }
 
 function Check() {
-    answer = document.getElementById("Ans").value;
+   
 }
